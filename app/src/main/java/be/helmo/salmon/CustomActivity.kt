2d79eb82a82658a95aa0209324553978a1d920dd
E181183 <d.choubassi@student.helmo.be>
2023-02-27 17:05:14 +0100

    boutons custom binding

--- app/src/main/java/be/helmo/salmon/CustomActivity.kt
+++ app/src/main/java/be/helmo/salmon/CustomActivity.kt
@@ -1,6 +1,8 @@
 package be.helmo.salmon
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import be.helmo.salmon.databinding.ActivityCustomBinding
 
@@ -10,8 +12,21 @@ class CustomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_custom)
+        //setContentView(R.layout.activity_custom)
         binding = ActivityCustomBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.redButton.setOnClickListener { view: View ->
+            Toast.makeText(this, "red", Toast.LENGTH_SHORT).show()
+        }
+        binding.greenButton.setOnClickListener { view: View ->
+            Toast.makeText(this, "green", Toast.LENGTH_SHORT).show()
+        }
+        binding.blueButton.setOnClickListener { view: View ->
+            Toast.makeText(this, "blue", Toast.LENGTH_SHORT).show()
+        }
+        binding.yellowButton.setOnClickListener { view: View ->
+            Toast.makeText(this, "yellow", Toast.LENGTH_SHORT).show()
+        }
     }
 }
