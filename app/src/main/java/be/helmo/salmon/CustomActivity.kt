59f859956cee5acdbd62d9ed16ef8fb6f5ead5cb
E181183 <d.choubassi@student.helmo.be>
2023-02-27 17:22:03 +0100

    boutons custom binding 2

--- app/src/main/java/be/helmo/salmon/CustomActivity.kt
+++ app/src/main/java/be/helmo/salmon/CustomActivity.kt
@@ -16,17 +16,50 @@ class CustomActivity : AppCompatActivity() {
         binding = ActivityCustomBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.resetButton.setOnClickListener { view: View ->
+            Toast.makeText(this, "reset", Toast.LENGTH_SHORT).show()
+        }
+
         binding.redButton.setOnClickListener { view: View ->
             Toast.makeText(this, "red", Toast.LENGTH_SHORT).show()
         }
+        binding.imageForRed.setOnClickListener { view: View ->
+            Toast.makeText(this, "red image", Toast.LENGTH_SHORT).show()
+        }
+        binding.soundForRed.setOnClickListener { view: View ->
+            Toast.makeText(this, "red sound", Toast.LENGTH_SHORT).show()
+        }
+
         binding.greenButton.setOnClickListener { view: View ->
             Toast.makeText(this, "green", Toast.LENGTH_SHORT).show()
         }
+        binding.imageForGreen.setOnClickListener { view: View ->
+            Toast.makeText(this, "green image", Toast.LENGTH_SHORT).show()
+        }
+        binding.soundForGreen.setOnClickListener { view: View ->
+            Toast.makeText(this, "green sound", Toast.LENGTH_SHORT).show()
+        }
+
         binding.blueButton.setOnClickListener { view: View ->
             Toast.makeText(this, "blue", Toast.LENGTH_SHORT).show()
         }
+        binding.imageForBlue.setOnClickListener { view: View ->
+            Toast.makeText(this, "blue image", Toast.LENGTH_SHORT).show()
+        }
+        binding.soundForBlue.setOnClickListener { view: View ->
+            Toast.makeText(this, "blue sound", Toast.LENGTH_SHORT).show()
+        }
+
         binding.yellowButton.setOnClickListener { view: View ->
             Toast.makeText(this, "yellow", Toast.LENGTH_SHORT).show()
         }
+        binding.imageForYellow.setOnClickListener { view: View ->
+            Toast.makeText(this, "yellow image", Toast.LENGTH_SHORT).show()
+        }
+        binding.soundForYellow.setOnClickListener { view: View ->
+            Toast.makeText(this, "yellow sound", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
